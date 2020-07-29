991fc6016a4f7e354083f18138c13aee6554a66b
Csaba Kiraly <csaba.kiraly@gmail.com>
2020-07-29 09:24:15 +0200

    improved PTQ with with representative dataset
    Added loading of the representative dataset required for the
    full integer quantization of weights and activations. Generate
    also a model with forced int8 ops.
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>

--- 02_convert.py
+++ 02_convert.py
@@ -8,6 +8,7 @@
 # import default python-library
 ########################################################################
 import os
+import glob
 import sys
 ########################################################################
 
@@ -18,6 +19,8 @@ import sys
 import common as com
 import keras_model
 import tensorflow as tf
+import numpy
+from tqdm import tqdm
 ########################################################################
 
 
@@ -26,6 +29,78 @@ import tensorflow as tf
 ########################################################################
 param = com.yaml_load()
 #######################################################################
+
+
+def list_to_vector_array(file_list,
+                         msg="calc...",
+                         n_mels=64,
+                         frames=5,
+                         n_fft=1024,
+                         hop_length=512,
+                         power=2.0):
+    """
+    convert the file_list to a vector array.
+    file_to_vector_array() is iterated, and the output vector array is concatenated.
+
+    file_list : list [ str ]
+        .wav filename list of dataset
+    msg : str ( default = "calc..." )
+        description for tqdm.
+        this parameter will be input into "desc" param at tqdm.
+
+    return : numpy.array( numpy.array( float ) )
+        vector array for training (this function is not used for test.)
+        * dataset.shape = (number of feature vectors, dimensions of feature vectors)
+    """
+    # calculate the number of dimensions
+    dims = n_mels * frames
+
+    # iterate file_to_vector_array()
+    for idx in tqdm(range(len(file_list)), desc=msg):
+        vector_array = com.file_to_vector_array(file_list[idx],
+                                                n_mels=n_mels,
+                                                frames=frames,
+                                                n_fft=n_fft,
+                                                hop_length=hop_length,
+                                                power=power)
+        if idx == 0:
+            dataset = numpy.zeros((vector_array.shape[0] * len(file_list), dims), float)
+        dataset[vector_array.shape[0] * idx: vector_array.shape[0] * (idx + 1), :] = vector_array
+
+    return dataset
+
+
+def file_list_generator(target_dir,
+                        dir_name="train",
+                        ext="wav"):
+    """
+    target_dir : str
+        base directory path of the dev_data or eval_data
+    dir_name : str (default="train")
+        directory name containing training data
+    ext : str (default="wav")
+        file extension of audio files
+
+    return :
+        train_files : list [ str ]
+            file list for training
+    """
+    com.logger.info("target_dir : {}".format(target_dir))
+
+    # generate training list
+    training_list_path = os.path.abspath("{dir}/{dir_name}/*.{ext}".format(dir=target_dir, dir_name=dir_name, ext=ext))
+    files = sorted(glob.glob(training_list_path))
+    if len(files) == 0:
+        com.logger.exception("no_wav_file!!")
+
+    com.logger.info("train_file num : {num}".format(num=len(files)))
+    return files
+########################################################################
+
+def representative_dataset_gen():
+  for _ in range(num_calibration_steps):
+    # Get sample input data as a numpy array in a method of your choosing.
+    yield [input]
 
 ########################################################################
 # main 02_convert.py
@@ -59,21 +134,55 @@ if __name__ == "__main__":
         model = keras_model.load_model(model_file)
         model.summary()
 
-        # Convert the model
+        # Convert the model to tflite
         converter = tf.lite.TFLiteConverter.from_keras_model(model)
         tflite_model = converter.convert()
-        converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
-        tflite_quant_model = converter.convert()
-
-        print("============== MODEL SAVE ==============")
-        # Save the TF Lite model.
         tflite_file = "{model}/model_{machine_type}.tflite".format(model=param["model_directory"],
                                                                     machine_type=machine_type)
         with tf.io.gfile.GFile(tflite_file, 'wb') as f:
             f.write(tflite_model)
 
-        tflite_quant_file = "{model}/model_{machine_type}_quant.tflite".format(model=param["model_directory"],
+        # Quantization of weights (but not the activations)
+        converter.optimizations = [tf.lite.Optimize.DEFAULT]
+        tflite_model = converter.convert()
+        tflite_file = "{model}/model_{machine_type}_quant.tflite".format(model=param["model_directory"],
                                                                     machine_type=machine_type)
-        with tf.io.gfile.GFile(tflite_quant_file, 'wb') as f:
-            f.write(tflite_quant_model)
+        with tf.io.gfile.GFile(tflite_file, 'wb') as f:
+            f.write(tflite_model)
 
+        print("============== DATASET_GENERATOR ==============")
+        files = file_list_generator(target_dir)
+        train_data = list_to_vector_array(files,
+                                          msg="generate train_dataset",
+                                          n_mels=param["feature"]["n_mels"],
+                                          frames=param["feature"]["frames"],
+                                          n_fft=param["feature"]["n_fft"],
+                                          hop_length=param["feature"]["hop_length"],
+                                          power=param["feature"]["power"])
+
+        def representative_dataset_gen():
+            for sample in train_data[::5]:
+                sample = numpy.expand_dims(sample.astype(numpy.float32), axis=0)
+                yield [sample]
+
+        # Full integer quantization of weights and activations
+        converter.optimizations = [tf.lite.Optimize.DEFAULT]
+        converter.representative_dataset = representative_dataset_gen
+        tflite_model = converter.convert()
+        tflite_file = "{model}/model_{machine_type}_quant_fullint.tflite".format(model=param["model_directory"],
+                                                                    machine_type=machine_type)
+        with tf.io.gfile.GFile(tflite_file, 'wb') as f:
+            f.write(tflite_model)
+
+
+        # Full integer quantization of weights and activations for micro
+        converter.optimizations = [tf.lite.Optimize.DEFAULT]
+        converter.representative_dataset = representative_dataset_gen
+        converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+        #converter.inference_input_type = tf.int8  # or tf.uint8
+        #converter.inference_output_type = tf.int8  # or tf.uint8
+        tflite_model = converter.convert()
+        tflite_file = "{model}/model_{machine_type}_quant_fullint_micro.tflite".format(model=param["model_directory"],
+                                                                    machine_type=machine_type)
+        with tf.io.gfile.GFile(tflite_file, 'wb') as f:
+            f.write(tflite_model)
