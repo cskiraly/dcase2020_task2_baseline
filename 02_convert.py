27a62f823852668e1449404948dddf240bd280b6
Csaba Kiraly <csaba.kiraly@gmail.com>
2020-07-30 02:00:05 +0200

    code refactoring
    Moving code common between several scripts to common.py
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>

--- 02_convert.py
+++ 02_convert.py
@@ -2,6 +2,7 @@
  @file   02_convert.py
  @brief  Script to convert model to tflite
  @author Csaba Kiraly
+ Copyright (C) 2020 Hitachi, Ltd. All right reserved.
 """
 
 ########################################################################
@@ -20,7 +21,6 @@ import common as com
 import keras_model
 import tensorflow as tf
 import numpy
-from tqdm import tqdm
 ########################################################################
 
 
@@ -30,77 +30,6 @@ from tqdm import tqdm
 param = com.yaml_load()
 #######################################################################
 
-
-def list_to_vector_array(file_list,
-                         msg="calc...",
-                         n_mels=64,
-                         frames=5,
-                         n_fft=1024,
-                         hop_length=512,
-                         power=2.0):
-    """
-    convert the file_list to a vector array.
-    file_to_vector_array() is iterated, and the output vector array is concatenated.
-
-    file_list : list [ str ]
-        .wav filename list of dataset
-    msg : str ( default = "calc..." )
-        description for tqdm.
-        this parameter will be input into "desc" param at tqdm.
-
-    return : numpy.array( numpy.array( float ) )
-        vector array for training (this function is not used for test.)
-        * dataset.shape = (number of feature vectors, dimensions of feature vectors)
-    """
-    # calculate the number of dimensions
-    dims = n_mels * frames
-
-    # iterate file_to_vector_array()
-    for idx in tqdm(range(len(file_list)), desc=msg):
-        vector_array = com.file_to_vector_array(file_list[idx],
-                                                n_mels=n_mels,
-                                                frames=frames,
-                                                n_fft=n_fft,
-                                                hop_length=hop_length,
-                                                power=power)
-        if idx == 0:
-            dataset = numpy.zeros((vector_array.shape[0] * len(file_list), dims), float)
-        dataset[vector_array.shape[0] * idx: vector_array.shape[0] * (idx + 1), :] = vector_array
-
-    return dataset
-
-
-def file_list_generator(target_dir,
-                        dir_name="train",
-                        ext="wav"):
-    """
-    target_dir : str
-        base directory path of the dev_data or eval_data
-    dir_name : str (default="train")
-        directory name containing training data
-    ext : str (default="wav")
-        file extension of audio files
-
-    return :
-        train_files : list [ str ]
-            file list for training
-    """
-    com.logger.info("target_dir : {}".format(target_dir))
-
-    # generate training list
-    training_list_path = os.path.abspath("{dir}/{dir_name}/*.{ext}".format(dir=target_dir, dir_name=dir_name, ext=ext))
-    files = sorted(glob.glob(training_list_path))
-    if len(files) == 0:
-        com.logger.exception("no_wav_file!!")
-
-    com.logger.info("train_file num : {num}".format(num=len(files)))
-    return files
-########################################################################
-
-def representative_dataset_gen():
-  for _ in range(num_calibration_steps):
-    # Get sample input data as a numpy array in a method of your choosing.
-    yield [input]
 
 ########################################################################
 # main 02_convert.py
@@ -151,8 +80,8 @@ if __name__ == "__main__":
             f.write(tflite_model)
 
         print("============== DATASET_GENERATOR ==============")
-        files = file_list_generator(target_dir)
-        train_data = list_to_vector_array(files,
+        files = com.file_list_generator(target_dir)
+        train_data = com.list_to_vector_array(files,
                                           msg="generate train_dataset",
                                           n_mels=param["feature"]["n_mels"],
                                           frames=param["feature"]["frames"],
--- common.py
+++ common.py
@@ -9,8 +9,11 @@
 # import python-library
 ########################################################################
 # default
+import csv
 import glob
 import argparse
+import itertools
+import re
 import sys
 import os
 
@@ -20,6 +23,7 @@ import librosa
 import librosa.core
 import librosa.feature
 import yaml
+from tqdm import tqdm
 
 ########################################################################
 
@@ -110,6 +114,13 @@ def file_load(wav_name, mono=False):
         logger.error("file_broken or not exists!! : {}".format(wav_name))
 
 
+def save_csv(save_file_path,
+             save_data):
+    with open(save_file_path, "w", newline="") as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerows(save_data)
+
+
 ########################################################################
 
 
@@ -188,3 +199,166 @@ def select_dirs(param, mode):
 
 ########################################################################
 
+
+def get_machine_id_list_for_test(target_dir,
+                                 dir_name="test",
+                                 ext="wav"):
+    """
+    target_dir : str
+        base directory path of "dev_data" or "eval_data"
+    test_dir_name : str (default="test")
+        directory containing test data
+    ext : str (default="wav)
+        file extension of audio files
+
+    return :
+        machine_id_list : list [ str ]
+            list of machine IDs extracted from the names of test files
+    """
+    # create test files
+    dir_path = os.path.abspath("{dir}/{dir_name}/*.{ext}".format(dir=target_dir, dir_name=dir_name, ext=ext))
+    file_paths = sorted(glob.glob(dir_path))
+    # extract id
+    machine_id_list = sorted(list(set(itertools.chain.from_iterable(
+        [re.findall('id_[0-9][0-9]', ext_id) for ext_id in file_paths]))))
+    return machine_id_list
+
+
+def test_file_list_generator(target_dir,
+                             id_name,
+                             mode,
+                             dir_name="test",
+                             prefix_normal="normal",
+                             prefix_anomaly="anomaly",
+                             ext="wav"):
+    """
+    target_dir : str
+        base directory path of the dev_data or eval_data
+    id_name : str
+        id of wav file in <<test_dir_name>> directory
+    dir_name : str (default="test")
+        directory containing test data
+    prefix_normal : str (default="normal")
+        normal directory name
+    prefix_anomaly : str (default="anomaly")
+        anomaly directory name
+    ext : str (default="wav")
+        file extension of audio files
+
+    return :
+        if the mode is "development":
+            test_files : list [ str ]
+                file list for test
+            test_labels : list [ boolean ]
+                label info. list for test
+                * normal/anomaly = 0/1
+        if the mode is "evaluation":
+            test_files : list [ str ]
+                file list for test
+    """
+    logger.info("target_dir : {}".format(target_dir+"_"+id_name))
+
+    # development
+    if mode:
+        normal_files = sorted(
+            glob.glob("{dir}/{dir_name}/{prefix_normal}_{id_name}*.{ext}".format(dir=target_dir,
+                                                                                 dir_name=dir_name,
+                                                                                 prefix_normal=prefix_normal,
+                                                                                 id_name=id_name,
+                                                                                 ext=ext)))
+        normal_labels = numpy.zeros(len(normal_files))
+        anomaly_files = sorted(
+            glob.glob("{dir}/{dir_name}/{prefix_anomaly}_{id_name}*.{ext}".format(dir=target_dir,
+                                                                                  dir_name=dir_name,
+                                                                                  prefix_anomaly=prefix_anomaly,
+                                                                                  id_name=id_name,
+                                                                                  ext=ext)))
+        anomaly_labels = numpy.ones(len(anomaly_files))
+        files = numpy.concatenate((normal_files, anomaly_files), axis=0)
+        labels = numpy.concatenate((normal_labels, anomaly_labels), axis=0)
+        logger.info("test_file  num : {num}".format(num=len(files)))
+        if len(files) == 0:
+            logger.exception("no_wav_file!!")
+        print("\n========================================")
+
+    # evaluation
+    else:
+        files = sorted(
+            glob.glob("{dir}/{dir_name}/*{id_name}*.{ext}".format(dir=target_dir,
+                                                                  dir_name=dir_name,
+                                                                  id_name=id_name,
+                                                                  ext=ext)))
+        labels = None
+        logger.info("test_file  num : {num}".format(num=len(files)))
+        if len(files) == 0:
+            logger.exception("no_wav_file!!")
+        print("\n=========================================")
+
+    return files, labels
+########################################################################
+
+def list_to_vector_array(file_list,
+                         msg="calc...",
+                         n_mels=64,
+                         frames=5,
+                         n_fft=1024,
+                         hop_length=512,
+                         power=2.0):
+    """
+    convert the file_list to a vector array.
+    file_to_vector_array() is iterated, and the output vector array is concatenated.
+
+    file_list : list [ str ]
+        .wav filename list of dataset
+    msg : str ( default = "calc..." )
+        description for tqdm.
+        this parameter will be input into "desc" param at tqdm.
+
+    return : numpy.array( numpy.array( float ) )
+        vector array for training (this function is not used for test.)
+        * dataset.shape = (number of feature vectors, dimensions of feature vectors)
+    """
+    # calculate the number of dimensions
+    dims = n_mels * frames
+
+    # iterate file_to_vector_array()
+    for idx in tqdm(range(len(file_list)), desc=msg):
+        vector_array = file_to_vector_array(file_list[idx],
+                                                n_mels=n_mels,
+                                                frames=frames,
+                                                n_fft=n_fft,
+                                                hop_length=hop_length,
+                                                power=power)
+        if idx == 0:
+            dataset = numpy.zeros((vector_array.shape[0] * len(file_list), dims), float)
+        dataset[vector_array.shape[0] * idx: vector_array.shape[0] * (idx + 1), :] = vector_array
+
+    return dataset
+
+
+def file_list_generator(target_dir,
+                        dir_name="train",
+                        ext="wav"):
+    """
+    target_dir : str
+        base directory path of the dev_data or eval_data
+    dir_name : str (default="train")
+        directory name containing training data
+    ext : str (default="wav")
+        file extension of audio files
+
+    return :
+        train_files : list [ str ]
+            file list for training
+    """
+    logger.info("target_dir : {}".format(target_dir))
+
+    # generate training list
+    training_list_path = os.path.abspath("{dir}/{dir_name}/*.{ext}".format(dir=target_dir, dir_name=dir_name, ext=ext))
+    files = sorted(glob.glob(training_list_path))
+    if len(files) == 0:
+        logger.exception("no_wav_file!!")
+
+    logger.info("train_file num : {num}".format(num=len(files)))
+    return files
+########################################################################
